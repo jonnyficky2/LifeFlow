1f83a2eaebac8bbf64a1edfc5fac29ceb1beee63
jonnyficky2 <jonnyficky2@gmail.com>
2026-06-14 23:08:27 +0700

    update sidebar and add transparant sidebar

--- js/navbar/navbar.js
+++ js/navbar/navbar.js
@@ -8,14 +8,16 @@ export function initSidebar() {
   const openSidebar = () => {
     sidebar?.classList.add("sidebar-open");
     overlay?.classList.add("sidebar-overlay-show");
+    toggle?.classList.add("active");
   };
 
   const closeSidebar = () => {
     sidebar?.classList.remove("sidebar-open");
     overlay?.classList.remove("sidebar-overlay-show");
+    toggle?.classList.remove("active");
   };
 
-  toggle?.addEventListener("click", openSidebar);
+  toggle?.addEventListener("click", () => sidebar?.classList.contains("sidebar-open") ? closeSidebar() : openSidebar());
   closeBtn?.addEventListener("click", closeSidebar);
   overlay?.addEventListener("click", closeSidebar);
   sidebarItems.forEach((item) => {
